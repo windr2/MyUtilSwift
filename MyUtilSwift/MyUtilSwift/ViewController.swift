a536153b0fe90f7220c0f52901007bba8cb2a3f8
SEO_MAC <seo_mac@SEO-MACui-MacBook-Pro.local>
2016-08-26 14:41:20 +0900

    8/26_1 util 시작 한다             끝까지 가보자~~ 하하하

---
+++ MyUtilSwift/MyUtilSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyUtilSwift
+//
+//  Created by SEO_MAC on 2016. 8. 26..
+//  Copyright © 2016년 SEO_MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
